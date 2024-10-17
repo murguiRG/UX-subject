7fe463fd82a57e3c7a65cb47d4a7e83d3a004db2
Regina Murguía <regimurguia@gmail.com>
2024-10-17 14:03:32 -0600

    Cards with static 'add to cart' button

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,46 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class AddToCart extends StatefulWidget {
+  const AddToCart({super.key});
+
+  @override
+  _AddToCartState createState() => _AddToCartState();
+}
+
+class _AddToCartState extends State<AddToCart> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        //* Shows a snackbar with a message
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Added to cart')),
+        );
+      },
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ButtonStyle(
+          shape: MaterialStateProperty.all<CircleBorder>(
+            const CircleBorder(
+              side: BorderSide.none,
+            ),
+          ),
+          backgroundColor: MaterialStateProperty.all<Color>(
+            Theme.of(context).colorScheme.secondary,
+          ),
+          minimumSize: MaterialStateProperty.all<Size>(const Size(45, 45)),
+          elevation: MaterialStateProperty.all<double>(3),
+        ),
+        child: const Icon(
+          Icons.add_rounded,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
+
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
 
@@ -54,35 +94,60 @@ class MyHomePage extends StatelessWidget {
               itemBuilder: (BuildContext context, int index) {
                 return Card(
                   color: colorShades[index],
-                  child: Column(
+                  child: Stack(
                     children: [
-                      const Expanded(
-                        flex: 16,
-                        child: Icon(
-                          Icons.add_photo_alternate_outlined,
-                          color: Colors.white,
-                        ),
-                      ),
-                      Expanded(
-                          flex: 10,
-                          child: Container(
-                            color: Colors.white,
-                            child: const Padding(
-                              padding: EdgeInsets.all(8),
-                              child: Align(
-                                  alignment: Alignment.topLeft,
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Text("Here are more details"),
-                                      Text("Brand or vendor"),
-                                      SizedBox(height: 10),
-                                      Text('\$100'),
-                                    ],
-                                  )),
+                      Column(
+                        children: [
+                          const Expanded(
+                            flex: 16,
+                            child: Icon(
+                              Icons.image_outlined,
+                              color: Colors.white,
+                              size: 40,
                             ),
-                          )),
+                          ),
+                          Expanded(
+                              flex: 10,
+                              child: Container(
+                                color: Colors.white,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8),
+                                  child: Align(
+                                      alignment: Alignment.topLeft,
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            "Product name",
+                                            style: TextStyle(
+                                                color: Colors.grey.shade600),
+                                          ),
+                                          const SizedBox(height: 5),
+                                          const Text(
+                                            "Brand or vendor",
+                                            style:
+                                                TextStyle(color: Colors.grey),
+                                          ),
+                                          const SizedBox(height: 20),
+                                          Text(
+                                            '\$100',
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.w500,
+                                                fontSize: 16,
+                                                color: Colors.grey.shade700),
+                                          ),
+                                        ],
+                                      )),
+                                ),
+                              )),
+                        ],
+                      ),
+                      const Positioned(
+                        top: 10,
+                        right: 0,
+                        child: AddToCart(),
+                      )
                     ],
                   ),
                 );
